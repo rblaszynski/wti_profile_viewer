db80607144320910165bdfa5ecb3129a24086cc7
Robert Błaszyński <robert.blaszynski@student.put.poznan.pl>
2020-01-12 11:57:43 +0100

    Add bootstrap

--- src/App.js
+++ src/App.js
@@ -3,6 +3,8 @@ import './App.css';
 import {Value, Image, List, useLDflex} from '@solid/react';
 import Email from './Email';
 import BlogPosts from './BlogPosts';
+import {Button, Card, Col, FormControl, InputGroup, Row} from "react-bootstrap";
+import Container from "react-bootstrap/Container";
 
 class App extends React.Component {
 
@@ -24,52 +26,78 @@ class App extends React.Component {
     render() {
         const {profile, posts, renderProfile} = this.state;
         return (
-            <div>
-                <h1>Profile viewer</h1>
-                <p>
-                    <label htmlFor="profile">Profile:</label>
-                    <input id="profile" value={profile} onChange={e => this.setState(
-                        {profile: e.target.value})}/>
-                    <button onClick={() => this.viewProfile(profile, posts)}>View</button>
-                </p>
-                {renderProfile &&
-                <div>
-                    <p>
-                        <Image className="profile-img" src={`[${profile}][foaf:img]`}/>
-                        <br/>
-                        <label htmlFor="name">Name: </label>
-                        <span id="name">
-                <Value src={`[${profile}][foaf:name]`}/>
-              </span>
-                        <br/>
-                        <label htmlFor="email">Email: </label>
-                        <span id="email">
-                <Email src={`[${profile}][foaf:mbox]`}/>
-              </span>
-                        <br/>
-                        <span>
-                            <label>Posty:</label>
-                        <BlogPosts author={posts}/>
-                        </span>
-                        <br/>
-                        <label htmlFor="friends">Friends: </label>
-                    </p>
-                    <List src={`[${profile}][foaf:knows]`}>{friend =>
-                        <li key={friend}>
-                            <Image className="profile-img" src={`[${friend}][foaf:img]`}/>
-                            <br/>
-                            <label>Name: </label>
-                            <button onClick={() => this.viewProfile(friend)}>
-                                <Value src={`[${friend}].name`}>{`${friend}`}</Value>
-                            </button>
-                            <br/>
-                            <label>Email: </label>
-                            <Email src={`[${friend}][foaf:mbox]`}/>
-                        </li>}
-                    </List>
-                </div>
-                }
-            </div>
+            <Container>
+                <Row>
+                    <Col xs={12}>
+                        <div>
+                            <h1>Profile viewer</h1>
+                            <InputGroup className="mb-3">
+                                <FormControl
+                                    placeholder="Recipient's username"
+                                    aria-label="Recipient's username"
+                                    aria-describedby="basic-addon2"
+                                    value={profile} onChange={e => this.setState(
+                                    {profile: e.target.value})}
+                                />
+                                <InputGroup.Append>
+                                    <Button onClick={() => this.viewProfile(profile, posts)}
+                                            variant="outline-secondary">View</Button>
+                                </InputGroup.Append>
+                            </InputGroup>
+                            {renderProfile && <Card>
+                                <Row>
+                                    <Col xs={6}>
+                                        <Card style={{ width: '' }}>
+                                            <Card.Body>
+                                                <Card.Title className="profile-title">
+                                                    <Value src={`[${profile}][foaf:name]`}/>
+                                                    <Image className="profile-img" src={`[${profile}][foaf:img]`}/>
+                                                </Card.Title>
+                                                <Card.Text>
+                                                    <Email src={`[${profile}][foaf:mbox]`}/>
+                                                </Card.Text>
+                                                <Card>
+                                                    <Card.Title>
+                                                        Posty użytkownika
+                                                    </Card.Title>
+                                                    <BlogPosts author={posts}/>
+                                                </Card>
+                                            </Card.Body>
+                                        </Card>
+                                    </Col>
+                                    <Col xs={6}>
+                                        <Card>
+                                            <Card.Img variant="top" />
+                                            <Card.Body>
+                                                <Card.Title>Friends</Card.Title>
+                                                <Card.Text>
+                                                    <List src={`[${profile}][foaf:knows]`}>{friend =>
+                                                        <Card style={{ padding: '10px' }}>
+                                                            <Image onClick={() => this.viewProfile(friend)} className="profile-img" src={`[${friend}][foaf:img]`}/>
+                                                            <Card.Title>
+                                                                <button onClick={() => this.viewProfile(friend)}>
+                                                                    <Value src={`[${friend}].name`}>{`${friend}`}</Value>
+                                                                </button>
+                                                            </Card.Title>
+                                                            <Card.Text>
+                                                                <Email src={`[${friend}][foaf:mbox]`}/>
+                                                            </Card.Text>
+                                                        </Card>
+                                                        }
+                                                    </List>
+                                                </Card.Text>
+                                            </Card.Body>
+                                        </Card>
+                                    </Col>
+                                </Row>
+                            <div>
+                            </div></Card>
+                            }
+                        </div>
+                    </Col>
+                </Row>
+            </Container>
+
         );
     }
 }
--- src/BlogPosts.jsx
+++ src/BlogPosts.jsx
@@ -1,38 +1,59 @@
 import React from 'react';
 import {Image, List, useLDflex, Value} from '@solid/react';
 import Email from "./Email";
+import Card from "react-bootstrap/Card";
+import Button from "react-bootstrap/Button";
 
-export default function BlogPosts({ author }) {
+export default function BlogPosts({author}) {
     console.log(author);
     let [posts, pending, error] = useLDflex(`[${author}][schema:blogPost]`, true);
     let [labels] = useLDflex(`[${author}][schema:blogPost].label`, true);
-    let [makers] = useLDflex(`[${author}][schema:blogPost].maker`, true);
+    let [keywords] = useLDflex(`[${author}][schema:blogPost][schema:keywords]`, true);
+    let [thumbnails] = useLDflex(`[${author}][schema:blogPost][schema:thumbnailUrl]`, true);
+    let [published] = useLDflex(`[${author}][schema:blogPost][schema:datePublished]`, true);
+    let [texts] = useLDflex(`[${author}][schema:blogPost][schema:text]`, true);
+
     if (pending)
         return <span>loading <em>({posts.length} posts so far)</em></span>;
     if (error)
         return <span>loading failed: {error.message}</span>;
-    posts = posts.map((post, index) => {
+    posts = posts.map((post) => {
         return post.value;
     });
-    labels = labels.map((label, index) => {
+    labels = labels.map((label) => {
         return label.value;
     });
-    makers = makers.map((maker) => {
-        return maker.value;
+    keywords = keywords.map((keyword) => {
+        return keyword.value.split(" ");
     });
-    let data = posts.map(function(x, i) {
-        return {name: labels[i], url: x}
+    thumbnails = thumbnails.map((thumbnail) => {
+        return thumbnail.value;
+    });
+    published = published.map((date) => {
+        return date.value;
+    });
+    texts = texts.map((text) => {
+        return text.value;
+    });
+    let data = posts.map(function (x, i) {
+        return {name: labels[i], url: x, keywords: keywords[i], thumbnail: thumbnails[i], date: published[i], text: texts[i]}
     });
     console.log(data);
     console.log(labels);
-    console.log(makers);
-    return <ul>{data.map((post, index) =>
-        <li key={index}>
-            <div>
-                <Value src={data[index].name}/>
-                <br/>
-                <a href={data[index].url} target="_blank">{data[index].url}</a>
-            </div>
-        </li>)}
-    </ul>;
+    console.log(keywords);
+    return data.map((post) => (<Card>
+        <Card.Img variant="top" src={post.thumbnail} />
+        <Card.Body>
+            <Card.Title>{post.name}</Card.Title>
+            <Card.Subtitle>{post.date}</Card.Subtitle>
+            <Card.Text>
+                {post.text}
+            </Card.Text>
+            <Card.Link href={post.url} target="_blank">Open post</Card.Link>
+        </Card.Body>
+    </Card>));
+
 }
+
+
+
